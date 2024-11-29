6847826a6148bcb38670ab6bdbc72e8a96196050
nakawodayo <dev.knakao@gmail.com>
2024-11-29 14:02:36 +0900

    remove: 不要なソースコードを削除

--- 04.graphql-insights/bff/src/graphql/graphql.module.ts
+++ 04.graphql-insights/bff/src/graphql/graphql.module.ts
@@ -1,15 +1,10 @@
 import { Module } from "@nestjs/common";
-import { CompaniesResolver } from "./companies/companies.resolver";
-import { CompanyResolver } from "./company/company.resolver";
-import { UserResolver } from "./user/user.resolver";
-import { UsersResolver } from "./users/users.resolver";
 import { ResidentModule } from './resident/resident.module';
 import { CityModule } from './city/city.module';
 import { PrefectureModule } from './prefecture/prefecture.module';
 import { PrefecturesModule } from './prefectures/prefectures.module';
 
 @Module({
-  providers: [CompaniesResolver, CompanyResolver, UsersResolver, UserResolver],
   imports: [ResidentModule, CityModule, PrefectureModule, PrefecturesModule],
 })
-export class GraphqlModule {}
+export class GraphqlModule { }
